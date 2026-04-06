b059aa51899f30a401432246241205a8913d024c
Masha20060902 <sivukmariaviktorovna@gmail.com>
2026-04-06 16:28:31 +0300

    Добавила День15

---
+++ Day15/Task/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+namespace Task
+{
+    public partial class MainWindow : Window
+    {
+        private VersionService versionService;
+        private readonly VersionControlViewModel _vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            versionService = new VersionService();
+            _vm = new VersionControlViewModel(versionService);
+            DataContext = _vm;
+            Loaded += MainWindow_Loaded;
+        }
+        public ObservableCollection<DocumentNode> Documents
+        => versionService.Documents;
+        public void _doc_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            MessageBox.Show("Добавлно", "", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+        }
+         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadingOverlay.Visibility = Visibility.Visible;
+            DataGrid.Visibility = Visibility.Hidden;
+            LoadProgress.IsIndeterminate = true;
+            LoadProgress.Value = 0;
+
+            var progress = new Progress<int>(p =>
+            {
+                LoadProgress.IsIndeterminate = false;
+                LoadProgress.Value = p;
+            });
+            await System.Threading.Tasks.Task.Delay(3000);
+            await versionService.LoadVersion(progress);
+            LoadingOverlay.Visibility = Visibility.Collapsed;
+            DataGrid.Visibility = Visibility.Visible;
+        }
+        private void DocumentsTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (DocumentsTreeView.SelectedItem is DocumentNode docum)
+            {
+                _vm.SelectedNode = docum;
+                versionService._document = docum;
+                DataGrid.ItemsSource = _vm.Versions;
+            }
+        }
+        private void AddVersionDoc_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new AddVersionDocument(this,versionService);
+            win.Owner = this;
+            win.ShowDialog();
+        }
+
+        private void DeleteVersion_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid.SelectedItem is Document selected)
+            {
+                if (MessageBox.Show("Вы точно хотите удалить этот элемент?",
+                 "Удаление версии",
+                 MessageBoxButton.YesNo,
+                 MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    versionService.DeleteVersion(selected);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала выберите версию", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            versionService.SaveVersion();
+        }
+
+        private void AddDoc_Click(object sender, RoutedEventArgs e)
+        {
+            string name = Microsoft.VisualBasic.Interaction.InputBox("Введите имя документа", "Новый документ", "Новый документ");
+            if (string.IsNullOrWhiteSpace(name))
+                return;
+            var newDoc = new DocumentNode { Name = name };
+            Documents.Add(newDoc);
+            versionService.SaveVersion();
+        }
+
+        private void ViewHistory_Click(object sender, RoutedEventArgs e)
+        {
+            if (versionService._document == null)
+            {
+                MessageBox.Show("Сначала выберите документ.", "История",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            var win = new HistoryWindow(versionService._document.Documents);
+            win.Owner = this;
+            win.ShowDialog();
+        }
+    }
+}
---
+++ Day15/Task/VersionService.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+namespace Task
+{
+    public class VersionService
+    {
+        public ObservableCollection<DocumentNode> _doc = new ObservableCollection<DocumentNode>();
+        public DocumentNode _document = new();
+        private string _path = "Kesh.data";
+        public ObservableCollection<DocumentNode> Documents => _doc;
+        public VersionService()
+        {
+        }
+        public async System.Threading.Tasks.Task LoadVersion(IProgress<int>? progress = null)
+        {
+            await System.Threading.Tasks.Task.Delay(3000);
+            if (!File.Exists(_path))
+                return;
+            var lines = await File.ReadAllLinesAsync(_path);
+            int total = lines.Length;
+            int processed = 0;
+            foreach (var line in lines)
+            {
+                var parts = line.Split(';');
+                string docName = parts[0];
+                int version = int.Parse(parts[1]);
+                DateTime date = DateTime.ParseExact(parts[2], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                string author = parts[3];
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var d = _doc.FirstOrDefault(x => x.Name == docName);
+                    if (d == null)
+                    {
+                        d = new DocumentNode { Name = docName };
+                        _doc.Add(d);
+                    }
+
+                    d.Documents.Add(new Document
+                    {
+                        VersionNumber = version,
+                        Date = date,
+                        Author = author
+                    });
+                });
+
+                processed++;
+                int percent = total == 0 ? 0 : processed * 100 / total;
+                progress?.Report(percent);
+            }
+        }
+        public void SaveVersion()
+        {
+            var lines = new ObservableCollection<string>();
+
+            foreach (var d in _doc)//
+            {
+                foreach (var v in d.Documents)
+                {
+                    lines.Add($"{d.Name};{v.VersionNumber};{v.Date:dd.MM.yyyy};{v.Author}");
+                }
+            }
+            File.WriteAllLines(_path, lines);
+        }
+        public void DeleteVersion(Document sender)
+        {
+            _document.Documents.Remove(sender);
+        }
+        public void AddVersion(int versionNumber, DateTime date, string author)
+        {
+            _document.Documents.Add(new Document
+            {
+                VersionNumber = versionNumber,
+                Date = date,
+                Author = author,
+            });
+        }
+    }
+}
