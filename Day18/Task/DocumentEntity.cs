052947b94cf004ff55d58e0966481d80ed2c3b63
Masha20060902 <sivukmariaviktorovna@gmail.com>
2026-04-09 16:52:03 +0300

    Добавила День18

---
+++ Day18/Task/DocumentEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Task
+{
+    public class DocumentEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CreatedAt { get; set; }
+        public ICollection<VersionEntity> Versions { get; set; } = new List<VersionEntity>();
+    }
+}
---
+++ Day18/Task/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Task
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
